e87562b1336e8f703ac7702565bd2224b662b087
jdgwartney <davidg@boundary.com>
2014-08-27 11:39:34 -0700

    Working version of portscan

--- scripts/portscan.py
+++ scripts/portscan.py
@@ -1,53 +1,88 @@
 #!/usr/bin/env python
+# Copyright 2014 Boundary, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from datetime import datetime
 import socket
 import subprocess
 import sys
-from datetime import datetime
 
-# Clear the screen
-subprocess.call('clear', shell=True)
+class PortScan:
+    
+  def __init__(self):
+        self.host = None
+        self.port = None
+        self.result = None
+        self.response = None
+        
+  def checkArgs(self, args):
+    if len(sys.argv) != 3:
+      self.usage()
+      sys.exit(1)
+    self.setHost(sys.argv[1])
+    self.setPort(int(sys.argv[2]))
+  
+  def usage():
+    sys.stdout.write("usage: {} <hostname> <port>\n".format(sys.argv[0]))
+    sys.stdout.write("\n")
+    sys.stdout.write("where:\n")
+    sys.stdout.write("   hostname - Host to check status of port\n")
+    sys.stdout.write("   port - Port to check status\n")
+  
+  def printMetric(self):
+    metricName = "PORT_AVAILABILITY"
+    if self.result == 0:
+      lresult = 1
+    else:
+      lresult = 0
+    print("PORT_AVAILABILITY {} {}".format(lresult, self.host))
+    print("PORT_RESPONSE {} {}".format(self.response, self.host))
 
-# Ask for input
-remoteServer    = raw_input("Enter a remote host to scan: ")
-remoteServerIP  = socket.gethostbyname(remoteServer)
+  def setHost(self, host):
+    self.host = host
+    
+  def setPort(self, port):
+    self.port = port
 
-# Print a nice banner with information on which host we are about to scan
-print "-" * 60
-print "Please wait, scanning remote host", remoteServerIP
-print "-" * 60
+  def execute(self):
+    self.checkArgs(sys.argv)
+    self.scan()
+    self.printMetric()
+    
+  def scan(self):
+    t1 = datetime.now()
+    try:
+      ip = socket.gethostbyname(self.host)
+      sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      self.result = sock.connect_ex((ip, self.port))
+      sock.close()
+    except KeyboardInterrupt:
+      print "You pressed Ctrl+C"
+      sys.exit()
+    except socket.gaierror:
+      print("Hostname could not be resolved. Exiting")
+    except socket.error:
+      print("Couldn't connect to server")
 
-# Check what time the scan started
-t1 = datetime.now()
+    # Checking the time again
+    t2 = datetime.now()
 
-# Using the range function to specify ports (here it will scans all ports between 1 and 1024)
+    # Calculates the difference of time, to see how long it took to run the script
+    t = t2 - t1
+    self.response = t.total_seconds() * 1000
 
-# We also put in some error handling for catching errors
+if __name__ == "__main__":
+  p = PortScan()
+  p.execute()
 
-try:
-    for port in range(1,1025):  
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((remoteServerIP, port))
-        if result == 0:
-            print "Port {}: \t Open".format(port)
-        sock.close()
-
-except KeyboardInterrupt:
-    print "You pressed Ctrl+C"
-    sys.exit()
-
-except socket.gaierror:
-    print 'Hostname could not be resolved. Exiting'
-    sys.exit()
-
-except socket.error:
-    print "Couldn't connect to server"
-    sys.exit()
-
-# Checking the time again
-t2 = datetime.now()
-
-# Calculates the difference of time, to see how long it took to run the script
-total =  t2 - t1
-
-# Printing the information to screen
-print 'Scanning Completed in: ', total
