a27c365ac78387219eac8f0aa7905a7d37512bc0
jdgwartney <davidg@boundary.com>
2014-09-02 14:28:32 -0700

    Change the metric names

--- scripts/cpu-load.sh
+++ scripts/cpu-load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo $(uptime) $(hostname) | awk '{printf("LOAD_1_MINUTE %s %s\nLOAD_5_MINUTE %s %s\nLOAD_15_MINUTE %s %s\n",$8,$11,$9,$11,$10,$11)}'
+echo $(uptime) $(hostname) | awk '{printf("BOUNDARY_CPU_LOAD_1_MINUTE %s %s\nBOUNDARY_CPU_LOAD_5_MINUTE %s %s\nBOUNDARY_CPU_LOAD_15_MINUTE %s %s\n",$8,$11,$9,$11,$10,$11)}'
