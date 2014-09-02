c6620e32884320b27ff2ea73564e6a67777a3abb
jdgwartney <davidg@boundary.com>
2014-09-02 15:54:21 -0700

    Not collecting loads correctly on CentOS

--- scripts/cpu-load.sh
+++ scripts/cpu-load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "$(uptime):$(hostname)" | cut -f 4,5 -d ':' | tr ':' ' ' | awk '{printf("BOUNDARY_CPU_LOAD_1_MINUTE %s %s\nBOUNDARY_CPU_LOAD_5_MINUTE %s %s\nBOUNDARY_CPU_LOAD_15_MINUTE %s %s\n",$1,$4,$2,$4,$3,$4)}'
+echo "$(uptime):$(hostname)" | cut -f 3 -d ',' | cut -f 2,3 -d ':' | tr ':' ' ' | awk '{printf("BOUNDARY_CPU_LOAD_1_MINUTE %s %s\nBOUNDARY_CPU_LOAD_5_MINUTE %s %s\nBOUNDARY_CPU_LOAD_15_MINUTE %s %s\n",$1,$4,$2,$4,$3,$4)}'
