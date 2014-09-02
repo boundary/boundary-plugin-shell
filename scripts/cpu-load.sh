1997c80ef25ef27f7d8af1f646c6190fb1737e40
jdgwartney <davidg@boundary.com>
2014-09-02 14:56:24 -0700

    Modify to work on Mac OS X and Centos/RedHat

--- scripts/cpu-load.sh
+++ scripts/cpu-load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo $(uptime) $(hostname) | awk '{printf("BOUNDARY_CPU_LOAD_1_MINUTE %s %s\nBOUNDARY_CPU_LOAD_5_MINUTE %s %s\nBOUNDARY_CPU_LOAD_15_MINUTE %s %s\n",$8,$11,$9,$11,$10,$11)}'
+echo "$(uptime):$(hostname)" | cut -f 4,5 -d ':' | tr ':' ' ' | awk '{printf("BOUNDARY_CPU_LOAD_1_MINUTE %s %s\nBOUNDARY_CPU_LOAD_5_MINUTE %s %s\nBOUNDARY_CPU_LOAD_15_MINUTE %s %s\n",$1,$4,$2,$4,$3,$4)}'
